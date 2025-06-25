c32eea6c84ad5c2e82174b795cb8f6b4fa9cec36
rexy <119789998+rexyka@users.noreply.github.com>
2025-06-26 03:47:44 +0700

    deploy1
    deploy done, postgres setup

--- app.py
+++ app.py
@@ -687,4 +687,4 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080,debug=True)
